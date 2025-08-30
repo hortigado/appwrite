90a012fc31591c0c4f731c739b732fb5897d0a01
Carlos Moises Arevalo <42924254+hortigado@users.noreply.github.com>
2025-08-30 13:31:33 -0500

    first commit

---
+++ lib/src/client_base.dart
@@ -0,0 +1,43 @@
+import 'response.dart';
+import 'client.dart';
+import 'enums.dart';
+
+abstract class ClientBase implements Client {
+  /// Your project ID
+  @override
+  ClientBase setProject(value);
+
+  /// Your secret JSON Web Token
+  @override
+  ClientBase setJWT(value);
+  @override
+  ClientBase setLocale(value);
+
+  /// The user session to authenticate with
+  @override
+  ClientBase setSession(value);
+
+  @override
+  ClientBase setSelfSigned({bool status = true});
+
+  @override
+  ClientBase setEndpoint(String endPoint);
+
+  @override
+  Client setEndPointRealtime(String endPoint);
+
+  @override
+  ClientBase addHeader(String key, String value);
+
+  @override
+  Future<String> ping();
+
+  @override
+  Future<Response> call(
+    HttpMethod method, {
+    String path = '',
+    Map<String, String> headers = const {},
+    Map<String, dynamic> params = const {},
+    ResponseType? responseType,
+  });
+}
